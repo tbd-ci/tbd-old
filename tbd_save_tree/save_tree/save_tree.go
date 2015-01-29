2b106cb2ae8d015fae10d64b8bcc406e4022b561
Jim Gaylard <jim.gaylard@rea-group.com>
2015-01-29 14:10:18 +1100

    Rewrite using git2go

--- tbd_save_tree/save_tree/save_tree.go
+++ tbd_save_tree/save_tree/save_tree.go
@@ -1,43 +1,37 @@
+// #include <index.h>
 package save_tree
 
 import (
-	"io/ioutil"
-	"os"
-	"os/exec"
-	"strings"
+	"C"
+
+	git "github.com/libgit2/git2go"
 )
 
-func strip(s string) string {
-	return strings.Replace(string(s), "\n", "", -1)
+var afterIndexCreated = func(string, string) int {
+	return 0
 }
 
 func Worktree() (string, error) {
 	// TODO: Handle the various errors which git can produce.
 	// Currently this just prints 'Error: Exit code <x>'.
 	// Probably want to show the error from git (e.g. no `.git` dir found)
-	env := os.Environ()
-	dir, err := ioutil.TempDir("", "tbd_index")
-	if err != nil {
-		return "", err
-	}
-	env = append(env, "GIT_INDEX_FILE="+dir+"/index")
 
-	_, err = withEnv(env, "git", "add", "-A", ".").CombinedOutput()
-
+	repo, err := git.OpenRepository(".")
 	if err != nil {
 		return "", err
 	}
 
-	worktree, err := withEnv(env, "git", "write-tree").Output()
+	index, err := git.NewIndex()
 	if err != nil {
 		return "", err
 	}
 
-	return strip(string(worktree)), nil
-}
+	index.AddAll([]string{"."}, git.GIT_INDEX_ADD_DEFAULT, afterIndexCreated)
 
-func withEnv(env []string, target ...string) *exec.Cmd {
-	c := exec.Command(target[0], target[1:]...)
-	c.Env = env
-	return c
+	treeOid, err := index.WriteTreeTo(repo)
+	if err != nil {
+		return "", err
+	}
+
+	return treeOid.String(), nil
 }
