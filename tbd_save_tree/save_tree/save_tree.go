1f45d8edf72b716086290e700ebe5273372f9c6a
Jim Gaylard <jgaylard75@gmail.com>
2015-02-01 09:15:30 +0000

    Further attempt

--- tbd_save_tree/save_tree/save_tree.go
+++ tbd_save_tree/save_tree/save_tree.go
@@ -2,46 +2,34 @@ package save_tree
 
 import (
 	"C"
+	"os"
 
 	git "github.com/libgit2/git2go"
 )
 
 func Worktree() (string, error) {
 
+	workdir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
 	repo, err := git.OpenRepository(".")
 	if err != nil {
 		return "", err
 	}
 
-	tree, err := repo.RevparseSingle("HEAD^{tree}")
+	index, err := repo.Index()
 	if err != nil {
 		return "", err
 	}
 
-	treeId := tree.Id()
-
-	entry := git.IndexEntry{
-		Mode: git.FilemodeTree,
-		Id:   treeId,
-		Path: ".",
-	}
-
-	index, err := git.NewIndex()
+	err = index.AddAll([]string{workdir}, git.IndexAddDefault, nil)
 	if err != nil {
 		return "", err
 	}
 
-	err = index.Add(&entry)
-	if err != nil {
-		return "", err
-	}
-
-	err = index.AddAll([]string{"."}, git.IndexAddDefault, nil)
-	if err != nil {
-		return "", err
-	}
-
-	treeOid, err := index.WriteTreeTo(repo)
+	treeOid, err := index.WriteTree()
 	if err != nil {
 		return "", err
 	}
