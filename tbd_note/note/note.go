02ac3b4deb4a92a4ab6b0b6e36f12dd9fb70d433
Jim Gaylard <jim.gaylard@rea-group.com>
2015-02-13 13:32:07 +1100

    First attempt at tbd_note
    tbd_note creates a note against the given tree. This will be used to
    signal success/failure of the build as well as other summary
    information.

---
+++ tbd_note/note/note.go
@@ -0,0 +1,77 @@
+package note
+
+import (
+	"fmt"
+	"time"
+
+	git "github.com/libgit2/git2go"
+)
+
+type User struct {
+	name  string
+	email string
+}
+
+func userDetails() (User, error) {
+	var user User
+
+	configPath, err := git.ConfigFindGlobal()
+	if err != nil {
+		return user, err
+	}
+
+	config, err := git.OpenOndisk(nil, configPath)
+	if err != nil {
+		return user, err
+	}
+
+	user.name, err = config.LookupString("user.name")
+	if err != nil {
+		return user, err
+	}
+	user.email, err = config.LookupString("user.email")
+	if err != nil {
+		return user, err
+	}
+
+	return user, err
+}
+
+func WriteNote(tree, buildResult *string) (string, error) {
+	repo, err := git.OpenRepository(".")
+	if err != nil {
+		return "", err
+	}
+
+	treeOid, err := git.NewOid(*tree)
+	if err != nil {
+		return "", err
+	}
+
+	var authorSig git.Signature
+
+	user, err := userDetails()
+	if err != nil {
+		return "", err
+	}
+
+	authorSig.Name = user.name
+	authorSig.Email = user.email
+	authorSig.When = time.Now()
+
+	// ref string, author, committer *Signature, id *Oid,
+	// note string, force bool
+	noteId, err := repo.CreateNote(
+		"",
+		&authorSig,
+		&authorSig,
+		treeOid,
+		fmt.Sprintf("%s at %v", *buildResult, time.Now()),
+		true,
+	)
+	if err != nil {
+		return "", err
+	}
+
+	return noteId.String(), nil
+}
