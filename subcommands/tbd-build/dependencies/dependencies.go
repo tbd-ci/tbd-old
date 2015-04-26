22a2d15a6807c584ea3959c84d270ffdd88c02e7
Jim Gaylard <jgaylard75@gmail.com>
2015-04-26 03:45:38 +0000

    WIP: further work on dependencies

--- subcommands/tbd-build/dependencies/dependencies.go
+++ subcommands/tbd-build/dependencies/dependencies.go
@@ -1,10 +1,6 @@
 package dependencies
 
-import (
-	"bufio"
-	"os"
-	"path/filepath"
-)
+import "fmt"
 
 type Dependencies struct {
 	dependent map[string][]string
@@ -12,39 +8,48 @@ type Dependencies struct {
 
 var dependencies Dependencies
 
-func Get(path string, done *Done) (*Dependencies, error) {
-	deps := make(map[string][]string)
-	err := filepath.Walk(path, func(taskPath string, info *os.FileInfo, err error) error {
-
-		task := filepath.Base(taskPath)
-
-		// Only run for each of "path/stage" subdirectories
-		if !info.IsDir() || filepath.Base(path) == task {
-			return nil
+func NextBatch(deps *Dependencies) (string, error) {
+	for key, vals := range deps.dependent {
+		if len(vals) == 0 {
+			return key, nil
 		}
-
-		file, err := os.Open(filepath.Join(taskPath, ".dependencies"))
-		if err != nil {
-			if os.IsNotExist(err) {
-				return nil
-			} else {
-				return err
-			}
-		}
-
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			if scanner.Text != "" {
-				deps[task] = append(deps[task], scanner.Text)
-			}
-		}
-
-	})(path)
-	if err != nil {
-		return err
 	}
-
-	deps := Dependencies{}
-
-	return
+	return "", fmt.Errorf("No dependencies or circular dependencies detected")
 }
+
+//func get(path string) (error, deps *Dependencies) {
+//  depTree := make(map[string][]string)
+//  err := filepath.Walk(path, func(taskPath string, info *os.FileInfo, err error) error {
+
+//    task := filepath.Base(taskPath)
+
+//    // Only run for each of "path/stage" subdirectories
+//    if !info.IsDir() || filepath.Base(path) == task {
+//      return nil
+//    }
+
+//    file, err := os.Open(filepath.Join(taskPath, ".dependencies"))
+//    if err != nil {
+//      if os.IsNotExist(err) {
+//        return nil
+//      } else {
+//        return err
+//      }
+//    }
+
+//    scanner := bufio.NewScanner(file)
+//    for scanner.Scan() {
+//      if scanner.Text != "" {
+//        deps[task] = append(deps[task], scanner.Text...)
+//      }
+//    }
+
+//  })(path)
+//  if err != nil {
+//    return err
+//  }
+
+//  deps := Dependencies{}
+
+//  return
+//}
--- subcommands/tbd-build/dependencies/dependencies_test.go
+++ subcommands/tbd-build/dependencies/dependencies_test.go
@@ -1,28 +1,74 @@
 package dependencies
 
-import (
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"testing"
-)
+import "testing"
 
-func TestGet(t *testing.T) {
-	dir, err := ioutil.TempDir("/tmp", "tbd-test")
+func TestNextBatch(t *testing.T) {
+	depTree := make(map[string][]string)
+
+	// dependencies a -> b --- a depends on b
+	// c -> a
+	// c -> b
+	// d -> a
+	// d -> b
+	// e -> c
+	// f -> d
+	// g -> e
+	// h -> f
+	// h -> e
+	// i -> g
+	// i -> h
+
+	depTree["taskA"] = []string{}
+	depTree["taskB"] = []string{}
+	depTree["taskC"] = []string{"taskA", "taskB"}
+	depTree["taskD"] = []string{"taskA", "taskB"}
+	depTree["taskE"] = []string{"taskC"}
+	depTree["taskF"] = []string{"taskD"}
+	depTree["taskG"] = []string{"taskE"}
+	depTree["taskH"] = []string{"taskE", "taskF"}
+	depTree["taskI"] = []string{"taskG", "taskH"}
+
+	deps := Dependencies{
+		dependent: depTree,
+	}
+
+	next, err := NextBatch(&deps)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	for s := range []string{"Astage1", "Astage2", "Bstage3", "Cstage4"} {
-		if err := Mkdir(filepath.Join(dir, s)); err != nil {
-			t.Fatal(err)
-		}
+	if next != "taskA" {
+		t.Errorf("%v should have been taskA", next)
 	}
 
-	file1, err := os.Create(file1path.Join(dir, "Bstage3"))
+	next, err = NextBatch(&deps)
 	if err != nil {
-		return err
+		t.Fatal(err)
 	}
-	defer file1.Close()
-	file1.Write([]byte("Astage1\nAstage2\n"))
+
+	// taskA complete, remove from depTree
+	if next != "taskB" {
+		t.Errorf("%v should have been [taskA, taskB]", next)
+	}
+
 }
+
+//func TestGet(t *testing.T) {
+//  dir, err := ioutil.TempDir("/tmp", "tbd-test")
+//  if err != nil {
+//    t.Fatal(err)
+//  }
+
+//  for s := range []string{"Astage1", "Astage2", "Bstage3", "Cstage4"} {
+//    if err := Mkdir(filepath.Join(dir, s)); err != nil {
+//      t.Fatal(err)
+//    }
+//  }
+
+//  file1, err := os.Create(file1path.Join(dir, "Bstage3"))
+//  if err != nil {
+//    return err
+//  }
+//  defer file1.Close()
+//  file1.Write([]byte("Astage1\nAstage2\n"))
+//}
